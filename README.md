8c41e2111caf6966b06ec192aeadab3e6bbbdef9
adhi <adhi.thirumala@gmail.com>
2025-01-29 01:55:14 -0600

    added readme changes describing stuff

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 # dotfiles
-Niri on Fedora Minimal with Framework Laptop 13
+BSPWM with Picom on Arch on Framework Laptop 13.
+Terminal: Alacritty
+Editor: Neovim
+Menu: rofi
+Other things: trackpad gestures, sxhkd
+
+I use polybar but default config lmao, haven't changed it yet.
+
---
+++ nvim/lua/config/lazy.lua
@@ -0,0 +1,381 @@
+-- Bootstrap lazy.nvim
+local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+if not (vim.uv or vim.loop).fs_stat(lazypath) then
+  local lazyrepo = "https://github.com/folke/lazy.nvim.git"
+  local out = vim.fn.system({ "git", "clone", "--filter=blob:none", "--branch=stable", lazyrepo, lazypath })
+  if vim.v.shell_error ~= 0 then
+    vim.api.nvim_echo({
+      { "Failed to clone lazy.nvim:\n", "ErrorMsg" },
+      { out,                            "WarningMsg" },
+      { "\nPress any key to exit..." },
+    }, true, {})
+    vim.fn.getchar()
+    os.exit(1)
+  end
+end
+vim.opt.rtp:prepend(lazypath)
+-- Make sure to setup `mapleader` and `maplocalleader` before
+-- loading lazy.nvim so that mappings are correct.
+-- This is also a good place to setup other settings (vim.opt)
+vim.g.mapleader = " "
+vim.g.maplocalleader = "\\"
+vim.cmd('set number')
+vim.cmd('set rnu')
+-- Setup lazy.nvim
+require("lazy").setup({
+  spec = {
+    -- import your plugins
+    { import = "plugins" },
+  },
+  -- Configure any other settings here. See the documentation for more details.
+  install = { colorscheme = { "gruvbox-material" } },
+  -- colorscheme that will be used when installing plugins.
+  -- automatically check for plugin updates
+  checker = { enabled = true },
+})
+
+
+local function should_enable_copilot()
+  local current_dir = vim.fn.getcwd()
+  local blocked_folders = {
+    "/home/adhi/cs128",
+    "/home/adhi/howdy-world-adhi-thirumala"
+    -- Add more paths here
+  }
+
+  for _, folder in ipairs(blocked_folders) do
+    if vim.startswith(current_dir, folder) then
+      -- Disable copilot for blocked folders
+      vim.cmd('Copilot disable')
+      return false
+    end
+  end
+
+  -- Enable copilot for all other folders
+  vim.cmd('Copilot enable')
+  return true
+end
+
+-- Set up an autocommand to check when changing directories
+vim.api.nvim_create_autocmd({ "DirChanged" }, {
+  callback = function()
+    should_enable_copilot()
+  end,
+})
+
+-- Check on startup
+should_enable_copilot()
+
+
+
+-- theme styles
+local virtual_env = function()
+  -- only show virtual env for Python
+  if vim.bo.filetype ~= 'python' then
+    return ""
+  end
+
+  local conda_env = os.getenv('CONDA_DEFAULT_ENV')
+  local venv_path = os.getenv('VIRTUAL_ENV')
+
+  if venv_path == nil then
+    if conda_env == nil then
+      return ""
+    else
+      return string.format("  %s (conda)", conda_env)
+    end
+  else
+    local venv_name = vim.fn.fnamemodify(venv_path, ':t')
+    return string.format("  %s (venv)", venv_name)
+  end
+end
+-- python venv status line for lualine
+
+-- lualine
+require('lualine').setup {
+  options = {
+    icons_enabled = true,
+    theme = 'auto',
+    component_separators = { left = '', right = '' },
+    section_separators = { left = '', right = '' },
+    disabled_filetypes = {
+      statusline = {},
+      winbar = {},
+    },
+    ignore_focus = {},
+    always_divide_middle = true,
+    always_show_tabline = true,
+    globalstatus = false,
+    refresh = {
+      statusline = 100,
+      tabline = 100,
+      winbar = 100,
+    }
+  },
+  sections = {
+    lualine_a = { 'mode' },
+    lualine_b = { 'branch', 'diff', 'diagnostics' },
+    lualine_c = { 'filename' },
+    lualine_x = { 'encoding', 'fileformat', 'filetype', 'virtual_env' },
+    lualine_y = { 'progress' },
+    lualine_z = { 'location' }
+  },
+  inactive_sections = {
+    lualine_a = {},
+    lualine_b = {},
+    lualine_c = { 'filename' },
+    lualine_x = { 'location' },
+    lualine_y = {},
+    lualine_z = {}
+  },
+  tabline = {},
+  winbar = {},
+  inactive_winbar = {},
+  extensions = {}
+}
+require 'colorizer'.setup {
+  '*',                      -- Highlight all files, but customize some others.
+  css = { rgb_fn = true, }, -- Enable parsing rgb(...) functions in css.
+  html = { names = false, } -- Disable parsing "names" like Blue or Gray
+}
+-- profile escalation
+vim.cmd('cmap w!! w !sudo tee "%"')
+-- nvim-tree
+require 'nvim-treesitter.configs'.setup {
+  -- A list of parser names, or "all" (the listed parsers MUST always be installed)
+  ensure_installed = { "c", "lua", "vim", "vimdoc", "query", "markdown", "markdown_inline" },
+
+  -- Install parsers synchronously (only applied to `ensure_installed`)
+  sync_install = false,
+
+  -- Automatically install missing parsers when entering buffer
+  -- Recommendation: set to false if you don't have `tree-sitter` CLI installed locally
+  auto_install = true,
+
+  -- List of parsers to ignore installing (or "all")
+
+  ---- If you need to change the installation directory of the parsers (see -> Advanced Setup)
+  -- parser_install_dir = "/some/path/to/store/parsers", -- Remember to run vim.opt.runtimepath:append("/some/path/to/store/parsers")!
+
+  highlight = {
+    enable = true,
+
+    -- NOTE: these are the names of the parsers and not the filetype. (for example if you want to
+    -- disable highlighting for the `tex` filetype, you need to include `latex` in this list as this is
+    -- the name of the parser)
+    -- list of language that will be disabled
+    -- Or use a function for more flexibility, e.g. to disable slow treesitter highlight for large files
+    disable = function(lang, buf)
+      local max_filesize = 100 * 1024 -- 100 KB
+      local ok, stats = pcall(vim.loop.fs_stat, vim.api.nvim_buf_get_name(buf))
+      if ok and stats and stats.size > max_filesize then
+        return true
+      end
+    end,
+
+    -- Setting this to true will run `:h syntax` and tree-sitter at the same time.
+    -- Set this to `true` if you depend on 'syntax' being enabled (like for indentation).
+    -- Using this option may slow down your editor, and you may see some duplicate highlights.
+    -- Instead of true it can also be a list of languages
+    additional_vim_regex_highlighting = false,
+  },
+  incremental_selection = {
+    enable = true,
+    keymaps = {
+      init_selection = "gnn", -- set to `false` to disable one of the mappings
+      node_incremental = "grn",
+      scope_incremental = "grc",
+      node_decremental = "grm",
+    },
+  },
+}
+require("mason").setup()
+require("mason-lspconfig").setup()
+local cmp = require 'cmp'
+require 'cmp'.setup {
+  sources = {
+    { name = 'nvim_lsp' }
+  },
+  mapping = cmp.mapping.preset.insert({
+    ['<C-b>'] = cmp.mapping.scroll_docs(-4),
+    ['<C-f>'] = cmp.mapping.scroll_docs(4),
+    ['<C-Space>'] = cmp.mapping.complete(),
+    ['<C-e>'] = cmp.mapping.abort(),
+    ['<CR>'] = cmp.mapping.confirm({ select = true }), -- Accept currently selected item. Set `select` to `false` to only confirm explicitly selected items.
+  }),
+}
+local cmp_autopairs = require('nvim-autopairs.completion.cmp')
+cmp.event:on(
+  'confirm_done',
+  cmp_autopairs.on_confirm_done()
+)
+-- The nvim-cmp almost supports LSP's capabilities so You should advertise it to LSP servers..
+-- make tab length two spaces for all lsp servers
+local o = vim.o
+
+o.expandtab = true
+o.smartindent = true
+o.tabstop = 2
+o.shiftwidth = 2
+local capabilities = require('cmp_nvim_lsp').default_capabilities()
+require 'lspconfig'.pyright.setup {
+  capabilities = capabilities
+}
+require 'lspconfig'.texlab.setup {
+  capabilities = capabilities
+}
+require 'lspconfig'.lua_ls.setup {
+  capabilities = capabilities
+}
+require 'lspconfig'.yamlls.setup {
+  capabilities = capabilities
+}
+require 'lspconfig'.ltex.setup {
+  capabilities = capabilities
+}
+require 'lspconfig'.ts_ls.setup {
+  capabilities = capabilities
+}
+require 'lspconfig'.eslint.setup {
+  capabilities = capabilities
+}
+require 'lspconfig'.autotools_ls.setup {
+  capabilities = capabilities
+}
+require 'lspconfig'.rust_analyzer.setup {
+  capabilities = capabilities
+}
+local on_attach_ocaml = function(client, bufnr)
+  -- Enable formatting on save
+  require("lsp-format").on_attach(client, bufnr)
+
+  -- Add any OCaml-specific keymaps or settings here
+  local opts = { noremap = true, silent = true, buffer = bufnr }
+  vim.keymap.set('n', 'gd', vim.lsp.buf.definition, opts)
+  vim.keymap.set('n', 'K', vim.lsp.buf.hover, opts)
+  vim.keymap.set('n', '<space>ca', vim.lsp.buf.code_action, opts)
+end
+
+require('lspconfig').ocamllsp.setup {
+  capabilities = capabilities,
+  on_attach = on_attach_ocaml,
+  filetypes = { "ocaml", "ocaml.menhir", "ocaml.interface", "ocaml.ocamllex", "reason", "dune" }
+}
+require 'lspconfig'.gopls.setup {
+  capabilities = capabilities,
+}
+require("lsp-format").setup {}
+
+require("lspconfig").pyright.setup { on_attach = require("lsp-format").on_attach }
+require("lspconfig").texlab.setup { on_attach = require("lsp-format").on_attach }
+require("lspconfig").lua_ls.setup { on_attach = require("lsp-format").on_attach }
+require("lspconfig").yamlls.setup { on_attach = require("lsp-format").on_attach }
+require("lspconfig").rust_analyzer.setup { on_attach = require("lsp-format").on_attach }
+
+require 'lspconfig'.clangd.setup {
+  capabilities = capabilities,
+  cmd = {
+    "clangd",
+    "--background-index",
+    "--clang-tidy",
+    "--header-insertion=iwyu",
+    "--completion-style=detailed",
+    "--function-arg-placeholders",
+    "--fallback-style=Google"
+  },
+  on_attach = require("lsp-format").on_attach
+}
+require("lspconfig").html.setup { on_attach = require("lsp-format").on_attach }
+require("lspconfig").ltex.setup { on_attach = require("lsp-format").on_attach }
+require("lspconfig").gopls.setup { on_attach = require("lsp-format").on_attach }
+require("lspconfig").eslint.setup { on_attach = require("lsp-format").on_attach }
+require("lspconfig").autotools_ls.setup { on_attach = require("lsp-format").on_attach }
+
+
+
+
+-- formatter.nvim config that uses default LSP to format on save and uses 2 spces instead of tabs
+local builtin = require('telescope.builtin')
+require('telescope').setup {
+  pickers = {
+    -- Default configuration for builtin pickers goes here:
+    -- picker_name = {
+    --   picker_config_key = value,
+    --   ...
+    -- }
+    -- Now the picker_config_key will be applied every time you call this
+    -- builtin picker
+  },
+  extensions = {
+    -- Your extension configuration goes here:
+    -- extension_name = {
+    --   extension_config_key = value,
+    -- }
+    -- please take a look at the readme of the extension you want to configure
+  }
+}
+
+-- map ctrl alt space to telescope
+vim.api.nvim_set_keymap('n', '<C-A-Space>', '<cmd>lua require("telescope.builtin").find_files()<CR>',
+  { noremap = true, silent = true })
+vim.api.nvim_set_keymap('n', '<C-A-b>', '<cmd>lua require("telescope.builtin").buffers()<CR>',
+  { noremap = true, silent = true })
+vim.api.nvim_set_keymap('n', '<C-A-w>', '<cmd>:CodeCompanionActions<CR>',
+  { noremap = true, silent = true })
+vim.api.nvim_set_keymap('n', '<C-A-d>', '<cmd>:CodeCompanionChat<CR>',
+  { noremap = true, silent = true })
+
+require('ibl').setup()
+
+
+require('nvim-ts-autotag').setup({
+  opts = {
+    -- Defaults
+    enable_close = true,          -- Auto close tags
+    enable_rename = true,         -- Auto rename pairs of tags
+    enable_close_on_slash = false -- Auto close on trailing </
+  },
+  -- Also override individual filetype configs, these take priority.
+  -- Empty by default, useful if one of the "opts" global settings
+  -- doesn't work well in a specific filetype
+})
+-- If you want insert `(` after select function or method item
+
+
+-- ai section
+require("codecompanion").setup({
+  adapters = {
+    copilot = function()
+      return require("codecompanion.adapters").extend("copilot", {
+        schema = {
+          model = {
+            default = "claude-3.5-sonnet",
+          },
+        },
+      })
+    end,
+  },
+  strategies = {
+    chat = {
+      slash_commands = {
+        ["file"] = {
+          callback = "strategies.chat.slash_commands.file",
+          description = "Select a file using Telescope",
+          opts = {
+            provider = "telescope", -- Other options include 'default', 'mini_pick', 'fzf_lua'
+            contains_code = true,
+          },
+        },
+      },
+    },
+  },
+  display = {
+    action_palette = {
+      provider = "telescope"
+    }
+  },
+})
+vim.opt.termguicolors = true
+require("bufferline").setup {}
+require("devcontainer").setup {}
+require('tcss').setup()
