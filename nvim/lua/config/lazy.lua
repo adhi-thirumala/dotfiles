81005c5cda86829d7fe76d7655fba0a19139a915
adhi <adhi.thirumala@gmail.com>
2025-02-26 03:50:59 -0600

    update Wed Feb 26 03:50:59 AM CST 2025

--- nvim/lua/config/lazy.lua
+++ nvim/lua/config/lazy.lua
@@ -355,7 +355,7 @@ require("codecompanion").setup({
       return require("codecompanion.adapters").extend("copilot", {
         schema = {
           model = {
-            default = "o3-mini-2025-01-31",
+            default = "claude-3.7-sonnet-thought",
           },
         },
       })
