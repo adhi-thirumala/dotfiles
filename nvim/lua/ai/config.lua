928bbd6133e25d2dbd1f5ed5b5581274d245b934
adhi <me@adhithirumala.com>
2026-01-05 17:20:43 -0500

    update Mon Jan  5 05:20:43 PM EST 2026

---
+++ nvim/lua/ai/config.lua
@@ -0,0 +1,24 @@
+-- Directories where Codeium should be disabled
+local disabled_dirs = {
+    "~/school",
+    "~/writing-a-c-compiler-sandler-rs"
+}
+
+local function check_and_disable_codeium()
+    local cwd = vim.fn.getcwd()
+
+    for _, dir in ipairs(disabled_dirs) do
+        if cwd:find(dir, 1, true) then
+            vim.cmd("CodeiumDisable")
+            break
+        end
+    end
+end
+
+-- Run check on startup
+check_and_disable_codeium()
+
+-- Also check when directory changes
+vim.api.nvim_create_autocmd("DirChanged", {
+    callback = check_and_disable_codeium,
+})
